2fa51c48e55e7998e426b25b17075f723615a5ec
felipetomazec <felipetomaz2206@gmail.com>
2023-05-20 20:53:22 -0300

    chore: init project

---
+++ src/main/java/com/github/felipetomazec/wallappapi/WallAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.felipetomazec.wallappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/felipetomazec/wallappapi/WallAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felipetomazec.wallappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
